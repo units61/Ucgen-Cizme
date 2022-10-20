da98e36bbd5abefe073743df88f80bf5e2f88c3a
Your Name <you@example.com>
2022-10-20 15:20:11 +0300

    Add Readme

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Üçgeni oluşturmak için boyu bilgisi giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+for(int a=0;a<sayi;a++)
+{				
+    for(int b=0;b<=a;b++)
+    {
+	    Console.Write("*");		
+	}
+		Console.Write("\n");
+}
+				
